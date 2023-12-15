1cb85885800ad10409e4761b2496a1380183eb1b
Blake Wilson <blake@aqualion.com>
2023-12-15 16:47:54 +0100

    Renamed Files

--- Nuclearcraft Reactor UI Control Panel/home/nReactorMultiBegin.lua
+++
@@ -1,7 +0,0 @@
-local component = require("component")
-local FissionReactorNames = {}
-
-for address in component.list('nc_fission_reactor') do
-	local reactor = component.get(address)
-	print(reactor)
-end
