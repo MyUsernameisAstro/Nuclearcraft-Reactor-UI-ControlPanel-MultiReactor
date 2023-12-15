d66e158352fb947e80ba92964d82e302ac97afa4
Blake Wilson <blake@aqualion.com>
2023-12-15 16:46:03 +0100

    Update nReactorMultiBegin.lua

--- Nuclearcraft Reactor UI Control Panel/home/nReactorMultiBegin.lua
+++ Nuclearcraft Reactor UI Control Panel/home/nReactorMultiBegin.lua
@@ -2,6 +2,6 @@ local component = require("component")
 local FissionReactorNames = {}
 
 for address in component.list('nc_fission_reactor') do
-	local reactor = component.proxy(address)
+	local reactor = component.get(address)
 	print(reactor)
 end
